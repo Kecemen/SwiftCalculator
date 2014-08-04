5aa9c96b1f4843ee240d58277df9e8e49a13249f
Gökhan Çokkeçeci <gokhancokkececi@Gokhan-MacBook-Pro.local>
2014-08-04 09:45:00 +0300

    Initial Commit

---
+++ SecondTrySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondTrySwift
+//
+//  Created by Gökhan Çokkeçeci on 4.08.2014.
+//  Copyright (c) 2014 Gökhan Çokkeçeci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
